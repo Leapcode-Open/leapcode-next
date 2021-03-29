9404092d9a5d8f5d811fb4b326b7ab86b25d6258
atulsuresh123 <atulsuresh123@gmail.com>
2021-03-29 11:39:50 +0530

    added config file for seo

---
+++ next-seo.config.js
@@ -0,0 +1,16 @@
+export default {
+    title:"Leapcode - Make your first open source contribution",
+    description:"Leapcode helps you contribute to open source projects right from your first pull request to working on major projects",
+    canonical:"https://www.leapcode.io/",
+    openGraph: {
+      type: 'website',
+      locale: 'en_IE',
+      url: 'https://www.leapcode.io/',
+      site_name: 'Leapcode',
+    },
+    twitter: {
+      handle: '@leapcodeio',
+      site: '@site',
+      cardType: 'summary_large_image',
+    },
+  };
